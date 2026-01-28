50f33d3ab80e5cf55704b45c5999439d5893abd6
Victor 72 <130411821+Vitor72@users.noreply.github.com>
2026-01-28 17:05:17 -0300

    modificaciones

--- ggj-2026/scenes/mask.gd
+++ ggj-2026/scenes/mask.gd
@@ -1,24 +1,25 @@
 extends Area2D
 
 @onready var mask = $"."
-@onready var spawn_1 : Marker2D = $"../Marker2D"
-@onready var spawn_2 : Marker2D = $"../Marker2D2"
-@onready var spawn_3 : Marker2D = $"../Marker2D3"
-@onready var spawn_4 : Marker2D = $"../Marker2D4"
-@onready var spawn_5 : Marker2D = $"../Marker2D5"
+@onready var spawn_1 : Area2D = %Spawns
+@onready var spawn_2 : Area2D = %Spawns2
+@onready var spawn_3 : Area2D = $"../Marker2D3"
+@onready var spawn_4 : Area2D = $"../Marker2D4"
+@onready var spawn_5 : Area2D = $"../Marker2D5"
+
 
 func _ready() -> void:
 	spawn_mask()
+	pass
 
+	
 func spawn_mask():
-	var posicion = randi_range(1,5)
+	var posicion = randi_range(1,2)
 	if posicion == 1:
 		mask.position = spawn_1.position
 	elif posicion == 2:
 		mask.position = spawn_2.position
-	elif posicion == 3:
-		mask.position = spawn_3.position
-	elif posicion == 4:
-		mask.position = spawn_4.position
-	elif posicion == 5:
-		mask.position = spawn_5.position
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("click"):
+		queue_free()
--- ggj-2026/scenes/prueba.gd
+++ ggj-2026/scenes/prueba.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
+@onready var marker_2d: Marker2D = $Marker2D
+var mask = preload("res://scenes/Mask.tscn")
+
+func  _physics_process(delta: float) -> void:
+	if mask == null:
+		mask.instantiate()
 
 
 
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("click"):
-		queue_free()
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
