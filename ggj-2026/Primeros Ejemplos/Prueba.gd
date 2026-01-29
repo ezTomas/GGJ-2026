6b3a2fc882f7b2ed0f3c08156e49200312349762
Enzo Tomas <tomasenzo03@gmail.com>
2026-01-28 23:05:01 -0300

    ajuste

--- ggj-2026/Primeros Ejemplos/Prueba.gd
+++ ggj-2026/Primeros Ejemplos/Prueba.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 
+
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton and event.pressed and $".".is_in_group("Mascara"):
 		on_click()
 
 func on_click():
