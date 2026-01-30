719869d0a7fb0cb4d89776410b8dbfdbc420a5c1
Enzo Tomas <tomasenzo03@gmail.com>
2026-01-29 23:28:49 -0300

    Menu Principal

---
+++ ggj-2026/scenes/Menu/menu_principal.gd
@@ -0,0 +1,16 @@
+extends Control
+@onready var musica_menu: AudioStreamPlayer2D = $Musica_Menu
+@onready var boton_jugar: AudioStreamPlayer2D = $BotonJugar
+
+func _ready() -> void:
+	musica_menu.play()
+	
+func _on_boton_jugar_pressed() -> void:
+	musica_menu.stop()
+	boton_jugar.play()
+	await  get_tree().create_timer(1.0).timeout
+	get_tree().change_scene_to_file("res://Primeros Ejemplos/escenario_prueba.tscn")
+
+
+func _on_boton_salir_pressed() -> void:
+	get_tree().quit()
